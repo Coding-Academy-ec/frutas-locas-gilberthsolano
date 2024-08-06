e619bb045fc8dc0bee5401fc69b395666d080b97
gilberthsolano <49965038+gilberthsolano@users.noreply.github.com>
2024-08-05 20:49:19 -0500

    cambios gibert

--- ejercicio.py
+++ ejercicio.py
@@ -1,19 +1,23 @@
+# ejercicio.py
+
 # Crea un diccionario llamado "frutas" con las siguientes parejas clave-valor:
-#               "manzana" - "roja"
-#               "banana" - "amarilla"
-#               "pera" - "verde"
-#               "naranja" - "naranja"
-# Imprime el diccionario "frutas" en la consola.
-# Imprime el valor asociado a la clave "banana" en la consola.
-# Imprime el valor asociado a la clave "uva" en la consola.
+# "manzana" - "roja"
+# "banana" - "amarilla"
+# "pera" - "verde"
+# "naranja" - "naranja"
 
 frutas = {
     "manzana": "roja",
-    "banana": , # banana lleva el valor "amarilla"
+    "banana": "amarilla",  # banana lleva el valor "amarilla"
     "pera": "verde",
-    "naranja": # naranja lleva el valor "naranja"
+    "naranja": "naranja"   # naranja lleva el valor "naranja"
 }
 
+# Imprime el diccionario "frutas" en la consola.
 print(frutas)
-print() # banana lleva el valor "amarilla"
-print(frutas.get("uva"))
+
+# Imprime el valor asociado a la clave "banana" en la consola.
+print(frutas["banana"])  # banana lleva el valor "amarilla"
+
+# Imprime el valor asociado a la clave "uva" en la consola.
+print(frutas.get("uva"))  # Esto imprimirá 'None' ya que "uva" no está en el diccionario
